054c401cae8a96675ce50800249bf7374b5f6018
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-08-22 00:03:43 +0100

    refactor - initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import hero from "./hero.png";
+import logo from "./logo.png";
+import PlasticVase from './plastic_vase.png';
+import EcoHouse from './house.png';
+import BottleCollection from "./bottle_collection.png";
+import home from './home.png';
+import plant from './plant.png';
+import recycle from './recycle.png';
+import depositor from './depositor.png';
+import craftsmen from './craftsmen.png';
+import collector from './collector.png';
+import globe from './globe.png';
+import PlasticBottles from './plastic_bottles.png';
+import bin from './bin.png';
+
+export {
+    hero,
+    logo,
+    PlasticVase,
+    EcoHouse,
+    BottleCollection,
+    home,
+    plant,
+    recycle,
+    depositor,
+    collector,
+    craftsmen,
+    globe,
+    PlasticBottles,
+    bin
+}
---
+++ src/assets/svg/index.js
@@ -0,0 +1,11 @@
+import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
+import { ReactComponent as FacebookIcon } from "./facebook.svg";
+import { ReactComponent as TwitterIcon } from "./twitter.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+
+export {
+    LinkedinIcon,
+    FacebookIcon,
+    TwitterIcon,
+    InstagramIcon
+}
