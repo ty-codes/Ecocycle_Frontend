d6ba56cd393cdd603ea680791aac572393090ae7
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-08-23 00:03:58 +0100

    refactor - add images and svgs

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,10 +2,17 @@ import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
 import { ReactComponent as FacebookIcon } from "./facebook.svg";
 import { ReactComponent as TwitterIcon } from "./twitter.svg";
 import { ReactComponent as InstagramIcon } from "./instagram.svg";
-
+import { ReactComponent as CFacebookIcon } from "./c-facebook.svg";
+import { ReactComponent as CGoogleIcon } from "./c-google.svg";
+import { ReactComponent as CameraIcon } from "./camera.svg";
+import { ReactComponent as ProfileIcon } from "./profile.svg";
 export {
     LinkedinIcon,
     FacebookIcon,
     TwitterIcon,
-    InstagramIcon
+    InstagramIcon,
+    CFacebookIcon,
+    CGoogleIcon,
+    CameraIcon,
+    ProfileIcon
 }
